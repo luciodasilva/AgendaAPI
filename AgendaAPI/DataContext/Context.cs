590a67110af766a7a783f5db45f76490637c69a7
Lúcio Silva <67482590+luciodasilva@users.noreply.github.com>
2022-04-24 14:32:45 -0300

    Update

--- AgendaAPI/DataContext/Context.cs
+++ AgendaAPI/DataContext/Context.cs
@@ -1,12 +1,26 @@
 ﻿using AgendaAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgendaAPI.DataContext
 {
-    public class Context
+    public class Context : DbContext
     {
+        public Context(DbContextOptions<Context> options) : base(options)
+        { }
         public DbSet <Horario> Horarios { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Funcionario> Funcionarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //faz o mapeamento do das classes mappings 
+            builder.ApplyConfigurationsFromAssembly(typeof(Context).Assembly);
+
+            foreach (var relationship in builder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(builder);
+        }
     }
 }
+
--- AgendaAPI/Models/Funcionario.cs
+++ AgendaAPI/Models/Funcionario.cs
@@ -3,11 +3,11 @@
     public class Funcionario
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int RegistroFuncionario { get; set; }
-        public string Cargo { get; set; }
-        public string Telefone { get; set; }
-        public Cliente Cliente { get; set; }
+        public string? Cargo { get; set; }
+        public string? Telefone { get; set; }
+        public Cliente? Cliente { get; set; }
 
     }
 }
